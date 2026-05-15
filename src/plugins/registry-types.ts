b768f85784124470e141ae2bedea0019d4db52d8
Peter Steinberger <steipete@gmail.com>
2026-05-15 08:35:20 +0100

    refactor(gateway): add method descriptor registry

--- src/plugins/registry-types.ts
+++ src/plugins/registry-types.ts
@@ -1,5 +1,6 @@
 import type { AgentHarness } from "../agents/harness/types.js";
 import type { ChannelPlugin } from "../channels/plugins/types.plugin.js";
+import type { GatewayMethodDescriptor } from "../gateway/methods/descriptor.js";
 import type { OperatorScope } from "../gateway/operator-scopes.js";
 import type { GatewayRequestHandlers } from "../gateway/server-methods/types.js";
 import type { HookEntry } from "../hooks/types.js";
@@ -444,6 +445,7 @@ export type PluginRegistry = {
   memoryEmbeddingProviders: PluginMemoryEmbeddingProviderRegistration[];
   agentHarnesses: PluginAgentHarnessRegistration[];
   gatewayHandlers: GatewayRequestHandlers;
+  gatewayMethodDescriptors: GatewayMethodDescriptor[];
   coreGatewayMethodNames?: string[];
   gatewayMethodScopes?: Partial<Record<string, OperatorScope>>;
   httpRoutes: PluginHttpRouteRegistration[];
